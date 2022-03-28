5b28b078647efc106f15ac48c7b17ca516ad3c2e
Bob-p- <poes997@gmail.com>
2022-03-29 00:17:16 +0100

    more c fun

--- fun.c
+++ fun.c
@@ -20,6 +20,9 @@ int main(){
   for(int i=0;i<10;i++)
     printf("%d",ndigit[i]);
   printf(" // Print out values from an array of ints");
+
+  printf("\nOur power function in action: power(2,8)=%d\n",power(2,8));
+
   printf("\n%d\nNow type some characters and if u hit the EOF i'll quit and tell you how many u typed\n",EOF);
   printf("^D to enter EOF\n");
   while((c=getchar())!=EOF){
@@ -31,6 +34,14 @@ int main(){
       ++nw;
   }
   printf("\n");
-  printf("Chars, words, line: %d, %d, %d\n", nc, nw, nl); // print number of chars, words and lines
+  printf("Chars, words, lines: %d, %d, %d\n", nc, nw, nl); // print number of chars, words and lines
+
   return 0;
+}
+
+int power(base,n){
+  int p=1;
+  for(int i=0;i<n;++i)
+    p*=base;
+  return p;
 }
