5e9687af1de60eeb309b6aef85d5233fa9789f99
Bob-p- <poes997@gmail.com>
2022-03-31 22:49:54 +0100

    seg fault fixed

--- pascals.c
+++ pascals.c
@@ -10,48 +10,50 @@ int fact(int n){
   return o;
 }
 
+int * pline(int n){
+  int m=n+1;
+  int * o = (int *) malloc(m*sizeof(int));
+  for(int r=0;r<n;r++){
+    o[r]=fact(n)/(fact(r)*(fact(n-r)));
+  }
+  return o;
+}
+
 /* Pascals triangle generator using dynamic memory allocation*/
 int** triangle(int h){
   int **o = (int **) malloc(h * sizeof(int *));
-  for(int n=0;n<=h;n++){
+  for(int n=0;n<h;n++){
     int c=n+1;
     o[n] = (int *) malloc(c * sizeof(int));
-    for(int r=0;r<=n;r++){
+    for(int r=0;r<n;r++){
       o[n][r]=fact(n)/(fact(r)*(fact(n-r)));
     }
   }
   return o;
 }
 
-int * pline(int n){
-  int m=n+1;
-  int * o = (int *) malloc(m*sizeof(int));
-  for(int r=0;r<=n;r++){
-    o[r]=fact(n)/(fact(r)*(fact(n-r)));
-  }
-  return o;
-}
 
 int main(){
-  int n=8;
+  int n=13;
   int ** t = triangle(n); 
-  for(int i=0;i<=n;i++){
-    for(int j=0;j<=i;j++){
+  for(int i=0;i<n;i++){
+    for(int j=0;j<i;j++){
       printf("%d ",t[i][j]);
     }
     printf("\n");
   }
-  for(int i=0;i<=n;i++)
+  for(int i=0;i<n;i++)
     free(t[i]);
   free(t);
 
   // Just one line to investigate bug
-  int m = 12;
-  int * l = pline(m);  
-  for(int i=0;i<=m;i++){
-    printf("%d ",l[i]);
-  }
-  printf("\n");
-  free(l);
+  //int m = 17;
+  //int * l = pline(m);  
+  //for(int i=0;i<=m;i++){
+  //  printf("%d ",l[i]);
+  //}
+  //printf("\n");
+  //free(l);
   return 0;
 }
+
