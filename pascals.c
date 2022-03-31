90c11950691e35cb889cdc146b61d21d5e1aa2d1
Bob-p- <poes997@gmail.com>
2022-03-31 21:19:39 +0100

    pascals somewhat working2

--- pascals.c
+++ pascals.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Factorial function */
 int fact(int n){
   int o=1;
   for(int i=1;i<n;i++){
@@ -9,30 +10,30 @@ int fact(int n){
   return o;
 }
 
+/* Pascals triangle generator using dynamic memory allocation*/
 int** triangle(int h){
   int **o = (int **) malloc(h * sizeof(int *));
-  for(int n=0;n<h;n++){
+  for(int n=0;n<=h;n++){
     int c=n+1;
     o[n] = (int *) malloc(c * sizeof(int));
-    for(int r=0;r<c;r++){
-      o[n][r]=fact(n)/(fact(r+1)*(fact(n-r-1)));
+    for(int r=0;r<=n;r++){
+      o[n][r]=fact(n)/(fact(r)*(fact(n-r)));
     }
   }
   return o;
 }
 
 int main(){
-  int n=5;
-  int ** o = triangle(n); 
-  for(int i=0;i<n;i++){
-    int l=sizeof(o[i])/sizeof(int);
-    for(int j=0;j<l;j++){
-      printf("%d ",o[i][j]);
+  int n=12;
+  int ** t = triangle(n); 
+  for(int i=0;i<=n;i++){
+    for(int j=0;j<=i;j++){
+      printf("%d ",t[i][j]);
     }
     printf("\n");
   }
   for(int i=0;i<n;i++)
-    free(o[i]);
-  free(o);
+    free(t[i]);
+  free(t);
   return 0;
 }
